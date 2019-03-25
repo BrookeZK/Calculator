80c35624585fca3ebeb1f5466f80e447d1af9005
Brooke Kullberg and Jake Nagel <jake@>
2019-03-25 15:13:12 -0700

    add celsius to fahrenheit conversion calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
-var bmi = function (q1,q2) {
-  // return weight703/heightSqr;
-var weight = parseInt(prompt(q1));
-var height = parseInt(prompt(q2));
-var weight703 = weight*703;
-var heightSqr = height*height;
-alert("Your Body Mass Index is " + weight703/heightSqr);
+var tempConversion = function (q1){
+  var celsius = parseInt(prompt(q1));
+  var farhenheit = ((9/5)*celsius)+32;
+  alert("the farhenheit temperature is " + farhenheit);
+
 };
 
+tempConversion("what is the celsius temperature?");
 
-bmi("What is your weight?","What is your height?");
+// var bmi = function (q1,q2) {
+//
+// var weight = parseInt(prompt(q1));
+// var height = parseInt(prompt(q2));
+// var weight703 = weight*703;
+// var heightSqr = height*height;
+// alert("Your Body Mass Index is " + weight703/heightSqr);
+// };
+
+
+//bmi("What is your weight?","What is your height?");
 
 
 
