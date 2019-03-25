974b6abb376aad925b25b6894ce28fd5ecf8cd2d
Brooke Kullberg and Jake Nagel <brooke@>
2019-03-25 16:17:22 -0700

    add hidden comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Celsius to Fahrenheit converter
 var tempConversion = function (q1){
   var celsius = parseInt(prompt(q1));
   var farhenheit = ((9/5)*celsius)+32;
@@ -7,28 +8,29 @@ var tempConversion = function (q1){
 
 tempConversion("what is the celsius temperature?");
 
-// var bmi = function (q1,q2) {
-//
-// var weight = parseInt(prompt(q1));
-// var height = parseInt(prompt(q2));
-// var weight703 = weight*703;
-// var heightSqr = height*height;
-// alert("Your Body Mass Index is " + weight703/heightSqr);
-// };
+//BMI Calculator
+var bmi = function (q1,q2) {
+
+var weight = parseInt(prompt(q1));
+var height = parseInt(prompt(q2));
+var weight703 = weight*703;
+var heightSqr = height*height;
+alert("Your Body Mass Index is " + weight703/heightSqr);
+};
 
 
-//bmi("What is your weight?","What is your height?");
+bmi("What is your weight?","What is your height?");
 
 
 
 
+//multiplier
+var multiply = function(number1, number2){
+	return number1 * number2;
+};
 
-// var multiply = function(number1, number2){
-// 	return number1 * number2;
-// };
-//
-// var number1 = parseInt(prompt("enter a number:"));
-// var number2 = parseInt(prompt("enter another number:"));
-// var results = multiply(number1, number2);
-//
-// alert(results);
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number:"));
+var results = multiply(number1, number2);
+
+alert(results);
