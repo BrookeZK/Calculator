04c25e7d1117387db000842132a9195b0f35d82b
Brooke Kullberg and Gavin Kimberlin <brooke@>
2019-03-27 14:15:35 -0700

    add jQuery javascript interactivity and add related css and js files into folder and link them to HTML file

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,60 @@
-// //Celsius to Fahrenheit converter
-var tempConversion = function (q1){
-  var celsius = parseInt(prompt(q1));
-  var farhenheit = ((9/5)*celsius)+32;
-  alert("the farhenheit temperature is " + farhenheit);
-
-};
-
-tempConversion("what is the celsius temperature?");
-
-// //BMI Calculator
-var bmi = function (q1,q2) {
-
-var weight = parseInt(prompt(q1));
-var height = parseInt(prompt(q2));
-var weight703 = weight*703;
-var heightSqr = height*height;
-alert("Your Body Mass Index is " + weight703/heightSqr);
-};
-
-
-bmi("What is your weight?","What is your height?");
-
-
-
-
 //calculator
+
+//Business (back-end) logic:
 var add = function(number1, number2){
 	return number1 + number2;
 };
 
 var subtract = function(number1, number2){
-	return number1 - number2;
+  return number1 - number2;
 };
 
 var multiply = function(number1, number2){
-	return number1 * number2;
+  return number1 * number2;
 };
 
 var divide = function(number1, number2){
-	return number1 / number2;
+  return number1 / number2;
 };
 
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
-//must change the variable next to (number1, number) below to change the calculator function)
-var results = divide(number1, number2);
+//Everything below this line is use interface (front-end) logic:
+$(document).ready(function(){
+  var number1 = parseInt(prompt("enter a number:"));
+  var number2 = parseInt(prompt("enter another number:"));
 
-alert(results);
+  var results = add(number1, number2);
+  alert(results);
+})
+
+//must change the variable next to (number1, number) below to change the calculator function)
+
+
+
+
+
+
+
+
+// // //Celsius to Fahrenheit converter
+// var tempConversion = function (q1){
+//   var celsius = parseInt(prompt(q1));
+//   var farhenheit = ((9/5)*celsius)+32;
+//   alert("the farhenheit temperature is " + farhenheit);
+//
+// };
+//
+// tempConversion("what is the celsius temperature?");
+//
+// // //BMI Calculator
+// var bmi = function (q1,q2) {
+//
+// var weight = parseInt(prompt(q1));
+// var height = parseInt(prompt(q2));
+// var weight703 = weight*703;
+// var heightSqr = height*height;
+// alert("Your Body Mass Index is " + weight703/heightSqr);
+// };
+//
+//
+// bmi("What is your weight?","What is your height?");
+//
